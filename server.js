8a1e256c138180866098adb39dfe747a938775df
Peter Bonchev <bonchp4@yahoo.com>
2018-03-26 16:54:45 +0100

    test

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req,res){
-  res.send(200, {'Content-Type': 'text/html'});
   var randomJoke = knockknock();
-  res.end(randomJoke);
+  res.send(randomJoke);
 });
