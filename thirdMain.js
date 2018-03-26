29035fd16740d204382ccc54bbc5b1e732f21ad2
Peter Bonchev <bonchp4@yahoo.com>
2018-03-26 16:41:51 +0100

    test

--- thirdMain.js
+++ thirdMain.js
@@ -1,7 +1,5 @@
-var http = require('http');
-var knockknock = require('knock-knock-jokes');
-http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/html'});
-var randomJoke = knockknock()
- res.end(randomJoke);
-}).listen(8080);
+app.get('/', function(req,res){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var randomJoke = knockknock();
+  res.end(randomJoke);
+});
