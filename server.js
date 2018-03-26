a4c082b277d9c935344f42c97af449a76ad482e0
Peter Bonchev <bonchp4@yahoo.com>
2018-03-26 16:38:57 +0100

    test

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 
 var http = require('http');
 var knockknock = require('knock-knock-jokes');
-app.get('/joke', function(req, res){
+http.createServer('/joke', function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
- var randomJoke = knockknock()
+  var randomJoke = knockknock()
   res.end(randomJoke);
-});
-app.listen(8080);
+}).listen(8080);
