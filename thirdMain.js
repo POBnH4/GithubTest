7d417a07025b80874038473379417593b76010b0
Peter Bonchev <bonchp4@yahoo.com>
2018-03-26 16:46:09 +0100

    Test

--- thirdMain.js
+++ thirdMain.js
@@ -1,4 +1,6 @@
-app.get('/', function(req,res){
+var express = require('express');
+var app = express();
+app.get('/joke', function(req,res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   var randomJoke = knockknock();
   res.end(randomJoke);
