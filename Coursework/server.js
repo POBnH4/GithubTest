fde86bc2457dbf9b185733d84aaeac3ee2a0526a
Peter Bonchev <bonchp4@yahoo.com>
2018-04-18 18:30:43 +0100

    test

--- Coursework/server.js
+++ Coursework/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
-app.use(exprees.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
