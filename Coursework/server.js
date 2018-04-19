308e498a080de25f5a8cb988a7f4a098c9ccc475
Peter Bonchev <bonchp4@yahoo.com>
2018-04-19 19:36:37 +0100

    asdasdasdas

--- Coursework/server.js
+++ Coursework/server.js
@@ -31,7 +31,7 @@ app.get('/', function(req,res) {
 
 app.post('/userDetails', function(req,res) {
 
-    db.collection('users').count({req.body.email})
+    db.collection('users').count({"email": req.body.email})
       .then((occurences) => {
          if(occurences > USER_DOES_NOT_EXIST){
            console.log('incorrect password or username');
